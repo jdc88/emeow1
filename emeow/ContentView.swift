4404a190a6479e39144c6af4a5c3428000374c68
Josephine <88jdchoi@gmail.com>
2023-06-23 18:46:44 -0700

    Initial Commit

---
+++ emeow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  emeow
+//
+//  Created by Josephine Choi on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ emeow/emeowApp.swift
@@ -0,0 +1,17 @@
+//
+//  emeowApp.swift
+//  emeow
+//
+//  Created by Josephine Choi on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct emeowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
